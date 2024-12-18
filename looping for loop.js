4cc68d8f9eb06c6507d150e764cf3afef97b77cb
DERMANRAY <rizkydermawan12345@gmail.com>
2024-12-18 16:42:53 +0700

    Add files via upload

---
+++ looping for loop.js
@@ -0,0 +1,16 @@
+// for(let i = 1; i <100; i++) {
+//     console.log(
+//         "Hello, World!"
+//     )
+// }
+
+// for(let i = 1; i < 100; i++) {
+//     console.log(i)
+// }
+
+//loop dengan array
+let NamaKucing = ['Yanto', 'Ujang', 'Ciprut', 'Malih', 'Asep']
+
+for(let i = 0; i < NamaKucing.length; i++) {
+    console.log(NamaKucing[i])
+}
